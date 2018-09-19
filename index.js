7b6f95a0ff540116b1d5ba0d42569c0e6b4c4035
SuzanneRashka <suzanne.rashka@gmail.com>
2018-09-18 20:27:38 -0500

    starting over

--- index.js
+++ index.js
@@ -5,53 +5,13 @@ var request = require("request");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var mongojs = require("mongojs");
+var morgan = require('morgan');
 
+var port = process.env.PORT || 3000;
 
 var $ = cheerio.load();
-let jsonframe = require('jsonframe-cheerio');
-jsonframe($) // initializing the plugin
-var app = express();
-
-// Database config
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
-
-//hook mongojs config to db 
-var db = mongojs(databaseUrl, collections);
-db.on("error", function (error) {
-    console.log("Database Error: ", error);
-});
-
-//main route
-app.get("/", function (req, res) {
-    res.send("Hello me");
-});
-
-app.get("/scrape", function (req, res) {
-    request("https://www.nytimes.com/", function (error, response, html) {
-
-        var $ = cheerio.load(html);
-
-
-        $("h2.story-heading").each(function (i, element) {
-
-            var result = {};
-
-            // Add the text and href of every link, and save them as properties of the result object
-            result.title = $(this).children("a").text();
-            result.link = $(this).children("a").attr("href");
-
-            if (result.title !== "" && result.link !== "") {
-                articlesArr.push(result);
-            }
-        });
-        callback(articlesArr);
-    });
-})
-
-
 
 
 app.listen(3000, function () {
-    console.log("Listening on port 3000")
+    console.log("Listening on port: " + port);
 });
